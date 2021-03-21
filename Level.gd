3b81d8443c813a7330ff867b370c49d9a29cba3c
Gecko05 <jacacent@gmail.com>
2021-03-20 21:39:36 -0600

    Added sound mechanics and bomb placement

---
+++ Level.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+export (PackedScene) var Spot
+export (PackedScene) var Bomb
+
+signal found_bomb
+
+const TILE_SIZE = 18
+const TILE_OFFSET_X = TILE_SIZE * 0.5
+const TILE_OFFSET_Y = TILE_SIZE * 1.5
+
+const MIN_GEIGER_PER = 0.15
+const MAX_GEIGER_PER = 0.75
+
+var dug_map = []
+var bomb_pos = [0, 0]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	dug_map.resize(9)
+	for i in range(9):
+		dug_map[i] = []
+		dug_map[i].resize(16)
+	for i in range(9):
+		for j in range(16):
+			dug_map[i][j] = false
+	randomize()
+	# Place the bomb in a random position
+	bomb_pos[0] = randi()%9
+	bomb_pos[1] = randi()%16
+	print(bomb_pos[0])
+	print(bomb_pos[1])
+	$Player.position.x = 7 * TILE_SIZE + 9
+	$Player.position.y = 4 * TILE_SIZE + 9
+
+func _on_Player_dig(pos):
+	var new_spot = Spot.instance()
+	if dug_map[pos[0]][pos[1]] == false:
+		$TileMap.add_child(new_spot)
+		new_spot.position.x = pos[1] * TILE_SIZE + TILE_OFFSET_X
+		new_spot.position.y = pos[0] * TILE_SIZE + TILE_OFFSET_Y
+		dug_map[pos[0]][pos[1]] = true
+	if pos[0] == bomb_pos[0] and pos[1] == bomb_pos[1]:
+		emit_signal("found_bomb")
+		print("Win the game")
+
+func _process(delta):
+	var player_pos = $Player.get_tile_pos()
+	var distance = abs(player_pos[0] - bomb_pos[0]) + abs(player_pos[1] - bomb_pos[1])
+	var new_period = (distance * 0.025) + 0.1
+	$GeigerTimer.wait_time = new_period
+
+func _on_GeigerTimer_timeout():
+	$GeigerClick.play()
--- Player.gd
+++ Player.gd
@@ -1,18 +1,38 @@
 extends KinematicBody2D
 
 const ACCELERATION = 8000
-const MAX_SPEED = 4000
+const MAX_SPEED = 90
 const FRICTION = 0.3
+
+signal dig(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	if Input.is_action_just_pressed("dig"):
+		$Sprite.animation = "dig"
+		var dig_pos = get_tile_pos()
+		emit_signal("dig", dig_pos)
+		return
+		
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var y_input = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	var motion = Vector2(0,0)
+	
+	if x_input == 0:
+		if y_input > 0:
+			$Sprite.animation = "south_walk"
+		elif y_input < 0:
+			$Sprite.animation = "north_walk"
+		elif y_input == 0:
+			$Sprite.animation = "south"
+	elif x_input > 0:
+		$Sprite.animation = "east_walk"
+	elif x_input < 0:
+		$Sprite.animation = "west_walk"
 	
 	if x_input != 0:
 		motion.x += x_input * ACCELERATION * delta
@@ -21,4 +41,8 @@ func _process(delta):
 		motion.y += y_input * ACCELERATION * delta
 		motion.y = clamp(motion.y, -MAX_SPEED, MAX_SPEED)
 	move_and_slide(Vector2(motion.x, motion.y))
-	
+
+func get_tile_pos():
+	var c = floor(position.x/18)
+	var r = floor(position.y/18)
+	return [r, c]
